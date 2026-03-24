38a2443d4d1bb7b452cd9aaec4e670877b8816d4
노트북\pp697 <pp6971@naver.com>
2026-03-24 20:24:19 +0900

    [Main] 언리얼 프로젝트 추가

---
+++ Source/Teamproject02/Teamproject02.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Teamproject02.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Teamproject02, "Teamproject02" );
